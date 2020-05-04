ea68d369490820a54b35df9de7a49805173bc4e7
tim.allan.london <tim.allan.london@gmail.com>
2020-05-04 06:40:31 +0100

    Update generate_pixel_configurations_COMBINATION_02.py

--- generate_pixel_configurations_COMBINATION_02.py
+++ generate_pixel_configurations_COMBINATION_02.py
@@ -36,29 +36,33 @@ def generate_random_configs(total_num_pixels, target_num_pixels_on):
     pixel_config = []                        # list to contain possible pixel configurations
     
     num_distinct_configs = combination(total_num_pixels, target_num_pixels_on)
-    print("number of distinct combinations:",num_distinct_configs)
+    print("number of distinct combinations:", num_distinct_configs)
     
-    while len(pixel_config) < combination(total_num_pixels, target_num_pixels_on): 
+    while len(pixel_config) < num_distinct_configs: 
         
         num_pixels_on = 0           
+
         random_configuration = []            # list contains a random confuration of pixels ON
-        
+
         
         for pixels in range (total_num_pixels):     
             
             randNum = random.randrange (0, 2)
             random_configuration.append (randNum)
+            
             num_pixels_on += randNum
             
         if num_pixels_on == target_num_pixels_on:               # have we found a configuration that equals target_num_pixels_on
             
             if random_configuration not in pixel_config:
-            
+                
                 pixel_config.append(random_configuration)       # Add to list of pixel configurations
    
     #print("combinations: ",pixel_config, end = "\n\n")
     return pixel_config
 
+
+    
 
 
 def split_configs(configs_to_split):
@@ -76,6 +80,7 @@ def split_configs(configs_to_split):
     return (split_pixel_config)
 
     
+
 
 def convert_list_to_ProcessingArray(configs_to_convert):
     
@@ -105,7 +110,10 @@ def write_to_file(processingArray):
     #print(f.read())
     
     
- 
+    
+    
+
+
 total_num_pixels = 6
 step = int(total_num_pixels / 3)
 
@@ -120,3 +128,11 @@ for num_pixels_ON in range (1, total_num_pixels, step):
 
 print(list_of_configs, end = "\n\n")
 
+
+
+
+
+
+
+
+  
